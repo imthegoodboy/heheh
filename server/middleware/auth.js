49a85c7c484bf12251ed4546ff97c858e5327ff6
Nikku...🤍 <nikku696969@gmail.com>
2026-06-29 17:33:48 +0530

    Fix auth and deployment issues

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,18 +3,18 @@ import jwt from 'jsonwebtoken'
 const auth = async (request, response, next) => {
     try {
         const token = request.cookies.accessToken || request?.headers?.authorization?.split(" ")[1]
-       
-        // Allow anyone to proceed with placing an order (no token check)
+
         if (!token) {
-            // You can either set userId as null or skip this part
-            request.userId = null;
-            return next(); // Proceed to the next middleware or route handler
+            return response.status(401).json({
+                message: "Unauthorized access",
+                error: true,
+                success: false
+            })
         }
 
-        // If a token exists, verify it
         const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN)
 
-        if (!decode) {
+        if (!decode?.id) {
             return response.status(401).json({
                 message: "Unauthorized access",
                 error: true,
@@ -26,8 +26,8 @@ const auth = async (request, response, next) => {
         next()
 
     } catch (error) {
-        return response.status(500).json({
-            message: "An error occurred during authentication", // Generic error message
+        return response.status(401).json({
+            message: "Unauthorized access",
             error: true,
             success: false
         })
